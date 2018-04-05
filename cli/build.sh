a5c6632ce3a0aa0d359d778c5f99e70d69f77378
Jan Burkl <jan.burkl@roguewave.com>
2018-04-05 15:06:03 +0200

    samll fix

--- cli/build.sh
+++ cli/build.sh
@@ -6,9 +6,11 @@ docker build -f mysql -t cli/mysql .
 
 docker build -f composer -t cli/composer .
 
+docker build -f bower -t cli/bower .
+
 echo
 echo Make sure that file 
 echo ~/.bash_profile
 echo exists and loads alias shell script like this:
-echo source /Users/jan/code/dockerfiles/cli/alias 
+echo . /Users/jan/code/dockerfiles/cli/alias 
 echo
